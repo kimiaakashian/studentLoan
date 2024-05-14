e391f1445e6925e429ee74b7d363618b03daf654
kimia <kimia@wiki-land.co>
2024-05-14 18:12:28 +0330

    commit all

--- src/main/java/org/example/utility/LoanMenu.java
+++ src/main/java/org/example/utility/LoanMenu.java
@@ -15,26 +15,30 @@ public class LoanMenu {
     LoanTypeService loanTypeService = ApplicationContext.getLoanTypeService();
     StudentService studentService = ApplicationContext.getStudentService();
     LoanRequestService loanRequestService = ApplicationContext.getLoanRequestService();
-    LoanInstallmentService loanInstallmentService =ApplicationContext.getLoanInstallmentService();
+    LoanInstallmentService loanInstallmentService = ApplicationContext.getLoanInstallmentService();
 
     public void loanRequest(long studentId) {
-        if (!isAllowedTimeToRegisterLoan()){
-            System.out.println("بازه زمانی غیر مجاز");}
-        else {
-        System.out.println("کد وام را وارد کنید ");
-        displayAllLoanTypes();
-        Long loanTypeId = scanner.nextLong();
-        System.out.println("مبلغ وام درخواستی را وارد کنید ");
-        Double loanAmount = scanner.nextDouble();
-        Student student = studentService.findById(studentId);
-        LoanType loanType = loanTypeService.findById(loanTypeId);
-        LoanRequest loanRequest = new LoanRequest(student, loanType, loanAmount, DateConvertorNew.todayDate());
-        String message = loanRequestService.saveLoanWithInstallments(loanRequest);
-        if (message.equalsIgnoreCase("OK"))
-            System.out.println("درخواست وام شما با موفقیت ثبت گردید.");
-        else
-            System.out.println(message);
-    }}
+        boolean isGraduate = studentService.IsGraduated(studentId);
+        if (isGraduate) {
+            System.out.println("شما فارق التحصیل شده اید و قادر به دریافت وام نمیباشید");
+        } else if (!isAllowedTimeToRegisterLoan()) {
+            System.out.println("بازه زمانی غیر مجاز است");
+        } else {
+            System.out.println("کد وام را وارد کنید ");
+            displayAllLoanTypes();
+            Long loanTypeId = scanner.nextLong();
+            System.out.println("مبلغ وام درخواستی را وارد کنید ");
+            Double loanAmount = scanner.nextDouble();
+            Student student = studentService.findById(studentId);
+            LoanType loanType = loanTypeService.findById(loanTypeId);
+            LoanRequest loanRequest = new LoanRequest(student, loanType, loanAmount, DateConvertorNew.todayDate());
+            String message = loanRequestService.saveLoanWithInstallments(loanRequest);
+            if (message.equalsIgnoreCase("OK"))
+                System.out.println("درخواست وام شما با موفقیت ثبت گردید.");
+            else
+                System.out.println(message);
+        }
+    }
 
     public void displayAllLoanTypes() {
         List<LoanType> loanTypes = loanTypeService.findAllLoanTypes();
@@ -51,6 +55,7 @@ public class LoanMenu {
             System.out.println((l.getId()) + ". " + l.getLoanAmount() + ". " + l.getLoanType() + ". " + l.getLoanDate());
         }
     }
+
     public void displayAllPaidInstallment(Long studentId) {
         List<LoanInstallment> loanInstallments = loanInstallmentService.findPaidInstallment(studentId);
         for (LoanInstallment l : loanInstallments
@@ -58,6 +63,7 @@ public class LoanMenu {
             System.out.println((l.getId()) + ". " + l.getInstallmentAmount() + ". " + l.getInstallmentDate() + ". " + l.getInstallmentStatus());
         }
     }
+
     public void displayAllUnPaidInstallment(Long studentId) {
         List<LoanInstallment> loanInstallments = loanInstallmentService.findUnPaidInstallment(studentId);
         for (LoanInstallment l : loanInstallments
@@ -67,14 +73,12 @@ public class LoanMenu {
     }
 
 
-
-
     public boolean isAllowedTimeToRegisterLoan() {
         String[] todayDateArr = DateConvertorNew.todayDate().split("/");
         int month = Integer.parseInt(todayDateArr[1]);
         int day = Integer.parseInt(todayDateArr[2]);
 
-        if ((month == 8 && day >= 1 && day <= 8) || (month == 2 && day >= 25 && day <= 30)){
+        if ((month == 8 && day >= 1 && day <= 8) || (month == 2 && day >= 25 && day <= 30)) {
             return true;
         }
         return false;
